2d5058603c78560a03fa1b10aea0547ceec6e378
jmarsal <jb.marsal@gmail.com>
2016-05-21 01:31:37 +0200

    push

--- get_next_line.h
+++ get_next_line.h
@@ -6,23 +6,15 @@
 /*   By: jmarsal  <jmarsal @student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/21 19:34:22 by jmarsal           #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2016/05/21 00:54:50 by jmarsal          ###   ########.fr       */
-=======
-/*   Updated: 2016/05/17 15:40:18 by jmarsal          ###   ########.fr       */
->>>>>>> master
+/*   Updated: 2016/05/21 01:28:40 by jmarsal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-<<<<<<< HEAD
-# define BUFF_SIZE 1
-# define EOL '\n'
-=======
 # define BUFF_SIZE 1024
+# define EOL '\n'
 
->>>>>>> master
 # include "libft.h"
 # include <unistd.h>
 
