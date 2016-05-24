ede08d4e21d2dd6921ac0464ce7195392edbd21a
Jean-baptiste MARSAL <jmarsal@student.42.fr>
2016-05-24 16:27:45 +0200

    push

--- get_next_line.c
+++ get_next_line.c
@@ -3,72 +3,35 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jmarsal  <jmarsal @student.42.fr>          +#+  +:+       +#+        */
+/*   By: jmarsal <jmarsal@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/21 19:47:53 by jmarsal           #+#    #+#             */
-/*   Updated: 2016/05/21 15:06:21 by jmarsal          ###   ########.fr       */
+/*   Updated: 2016/05/24 16:23:31 by jmarsal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static t_line	*add_elem(t_line **list, int fd)
+t_gnl		*new_file(const int fd)
 {
-	t_line	*new;
-	t_line	*cur;
+	t_gnl	*new_link;
 
-	if (!(new = (t_line *)malloc(sizeof(t_line))))
+	if (!(new_link = (t_gnl*)malloc(sizeof(t_gnl))))
 		return (NULL);
-	new->fd = fd;
-	new->line = ft_strnew(0);
-	new->next = NULL;
-	if (*list == NULL)
-		*list = new;
-	else
-	{
-		cur = *list;
-		while (cur->next)
-			cur = cur->next;
-		cur->next = new;
-	}
-	return (new);
+	new_link->fd = fd;
+	new_link->line = ft_strnew(0);
+	new_link->next = NULL;
+	return (new_link);
 }
 
-char		*get_line(t_line *list, char **line)
-{
-	char	*tmp;
-	char	*text;
-	int		i;
-
-//	tmp = NULL;
-	i = 0;
-	text = list->line;
-	while (text[i])
-	{
-		if (text[i] == EOL)
-		{
-			*line = ft_strsub(text, 0, i);
-			tmp = text;
-			text = ft_strdup(text + (i + 1));
-			free(tmp);
-			return (text);
-		}
-		i++;
-	}
-	*line = ft_strdup(text);
-	ft_strclr(text);
-	ft_strclr(list->line);
-	//free(list->line);
-	return (text);
-}
-
-static	int		read_file(int fd, t_line *list)
+int			read_file(int fd, t_gnl *list)
 {
 	int		ret;
 	char	buf[BUFF_SIZE + 1];
 	char	*tmp;
 
-//	tmp = NULL;
+	tmp = NULL;
+	ret = -42;
 	while (!ft_strchr(list->line, EOL))
 	{
 		if ((ret = read(fd, buf, BUFF_SIZE)) < 0)
@@ -87,30 +50,56 @@ static	int		read_file(int fd, t_line *list)
 	return (ret);
 }
 
-int				get_next_line(const int fd, char **line)
+char		*get_line(t_gnl *list, char **line)
 {
-	static	t_line		*file;
-	int					ret;
-	t_line				*tmp;
+	char	*tmp;
+	char	*text;
+	int		i;
 
-	//tmp = NULL;
-	if (!file)
-		file = add_elem(&file, fd);
-	tmp = file;
+	tmp = NULL;
+	i = 0;
+	text = list->line;
+	while (text[i])
+	{
+		if (text[i] == EOL)
+		{
+			*line = ft_strsub(text, 0, i);
+			tmp = text;
+			text = ft_strdup(text + (i + 1));
+			free(tmp);
+			return (text);
+		}
+		i++;
+	}
+	*line = ft_strdup(text);
+	ft_strclr(text);
+	ft_strclr(list->line);
+	return (text);
+}
+
+int			get_next_line(int const fd, char **line)
+{
+	static	t_gnl		*list_files;
+	int					ret;
+	t_gnl				*current_file;
+
+	if (!list_files)
+		list_files = new_file(fd);
+	current_file = list_files;
 	if (fd < 0 || !line)
 		return (-1);
-	while (tmp)
+	while (current_file)
 	{
-		if (tmp->fd == fd)
+		if (current_file->fd == fd)
 			break ;
-		if (tmp->next == NULL)
-			add_elem(&file, fd);
-		tmp = tmp->next;
+		if (current_file->next == NULL)
+			ft_lstadd((t_list**)&current_file, (t_list*) new_file(fd));
+		current_file = current_file->next;
 	}
-	if ((ret = read_file(fd, tmp)) == -1)
+	if ((ret = read_file(fd, current_file)) == -1)
 		return (-1);
-	tmp->line = get_line(tmp, line);
-	if (!ft_strlen(tmp->line) && !ft_strlen(*line) && !ret)
+	current_file->line = get_line(current_file, line);
+	if (!ft_strlen(current_file->line) && !ft_strlen(*line) && !ret)
 		return (0);
 	else
 		return (1);
--- get_next_line.h
+++ get_next_line.h
@@ -3,27 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jmarsal  <jmarsal @student.42.fr>          +#+  +:+       +#+        */
+/*   By: jmarsal <jmarsal@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/21 19:34:22 by jmarsal           #+#    #+#             */
-/*   Updated: 2016/05/21 14:56:47 by jmarsal          ###   ########.fr       */
+/*   Updated: 2016/05/24 15:11:08 by jmarsal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE 64
+# define BUFF_SIZE 1
 # define EOL '\n'
 
 # include "libft.h"
 # include <unistd.h>
 
-typedef struct		s_line
+typedef struct		s_gnl
 {
 	int				fd;
 	char			*line;
-	struct s_line	*next;
-}					t_line;
+	struct s_gnl	*next;
+}					t_gnl;
 
 int					get_next_line(const int fd, char **line);
 
