1dcee3e8ae9172c7f762693eeb4f057373927746
Jean-baptiste MARSAL <jmarsal@student.42.fr>
2016-06-06 14:07:27 +0200

    Modification du main de test
    --> Modification du main de test pour pouvoir utilise l'entree standard

--- launch/main.c
+++ launch/main.c
@@ -6,7 +6,7 @@
 /*   By: jmarsal <jmarsal@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/19 13:56:22 by vquesnel          #+#    #+#             */
-/*   Updated: 2016/05/24 11:05:02 by jmarsal          ###   ########.fr       */
+/*   Updated: 2016/06/06 14:05:27 by jmarsal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,13 +21,27 @@ int		main(int ac, char **av)
 	char	*line;
 
 	line = NULL;
-	fd = open(av[ac - 1], O_RDONLY);
-	while ((ret = get_next_line(fd, &line) > 0))
+	if (ac < 2)
 	{
-		ft_putstr(line);
-		ft_putstr("\033[32;1m|---> \033[34;1m");
-		ft_putnbr(ret);
-		ft_putstr("\n\033[0m");
+		fd = 0;
+		while ((ret = get_next_line(fd, &line) > 0))
+		{
+			ft_putstr(line);
+			ft_putstr("\033[32;1m|---> \033[34;1m");
+			ft_putnbr(ret);
+			ft_putstr("\n\033[0m");
+		}
+	}
+	else
+	{
+		fd = open(av[ac - 1], O_RDONLY);
+		while ((ret = get_next_line(fd, &line) > 0))
+		{
+			ft_putstr(line);
+			ft_putstr("\033[32;1m|---> \033[34;1m");
+			ft_putnbr(ret);
+			ft_putstr("\n\033[0m");
+		}
 	}
 	ft_putstr("\n");
 	ft_putstr("\033[31;1;4mFINAL RET\033[0m\033[31;1m |---> \033[34;1m");
